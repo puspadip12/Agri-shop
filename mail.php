153458ccbd6f701a50ef6caa371ed3529723f7e4
PUSPADIP CHANDRA THAKUR <73663889+puspadip12@users.noreply.github.com>
2023-10-03 00:06:08 +0530

    update

--- mail.php
+++ mail.php
@@ -21,7 +21,7 @@
 	
 	ob_start();
 	?>
-		Hi imransdesign!<br /><br />
+		Hello!<br /><br />
 		<?php echo ucfirst( $name ); ?>  has sent you a message via contact form on your website!
 		<br /><br />
 		
@@ -38,7 +38,7 @@
 	$body = ob_get_contents();
 	ob_end_clean();
 	
-	$to = 'support@fruitkha.com';
+	$to = 'puspadip.pt@gmail.com';
 
 	$s = mail($to,$subject,$body,$headers,"-t -i -f $from");
 
