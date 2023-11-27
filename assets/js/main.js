60f031d1d1a78d686a2f380e88fed18b0025c351
PUSPADIP CHANDRA THAKUR <73663889+puspadip12@users.noreply.github.com>
2023-11-27 22:01:34 +0530

    1001

--- assets/js/main.js
+++ assets/js/main.js
@@ -75,16 +75,6 @@
             }
         });
 
-        // count down
-        if ($('.time-countdown').length) {
-            $('.time-countdown').each(function () {
-                var $this = $(this), finalDate = $(this).data('countdown');
-                $this.countdown(finalDate, function (event) {
-                    var $this = $(this).html(event.strftime('' + '<div class="counter-column"><div class="inner"><span class="count">%D</span>Days</div></div> ' + '<div class="counter-column"><div class="inner"><span class="count">%H</span>Hours</div></div>  ' + '<div class="counter-column"><div class="inner"><span class="count">%M</span>Mins</div></div>  ' + '<div class="counter-column"><div class="inner"><span class="count">%S</span>Secs</div></div>'));
-                });
-            });
-        }
-
         // projects filters isotop
         $(".product-filters li").on('click', function () {
 
